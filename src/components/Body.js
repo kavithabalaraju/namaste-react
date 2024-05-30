3bb7104079888bb8f5646852c31f6247b38aecc5
kavitha <chkavitha324@gmail.com>
2024-05-30 15:14:14 +0530

    commiting the structure changes

---
+++ src/components/Body.js
@@ -0,0 +1,16 @@
+import ResturantCard from "./ResturantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+    return (<div className="body">
+        <div className="search">Search</div>
+        <div className="res-container">
+            {resList.map((restaurant) => 
+                (<ResturantCard key = {restaurant.info.id} resData = {restaurant}/>))};
+
+        </div>
+    </div>
+
+    );
+}
+export default Body;
---
+++ src/components/ResturantCard.js
@@ -0,0 +1,17 @@
+import { CDN_URL} from "../utils/constants";
+const ResturantCard = (props) => {
+    const {resData} = props;
+    console.log(resData);
+    const {cloudinaryImageId,name,cuisines,avgRating} = resData?.info;
+    const {deliveryTime} = resData?.info?.sla;
+    return (<div className="resCard">
+        <img className="rest-logo" alt = "res-logo" src={ CDN_URL+cloudinaryImageId}></img>
+        
+        <h3>{name}</h3>
+        <h4>{cuisines.join(", ")}</h4>
+        <h5>{avgRating} Stars</h5>
+        <h5>{deliveryTime} minutes</h5>
+    </div>);
+
+}
+export default ResturantCard;
