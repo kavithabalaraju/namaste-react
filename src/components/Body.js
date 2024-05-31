7fc34e41d7137c502cb1237fb2693b21d231c4d0
kavitha <chkavitha324@gmail.com>
2024-05-31 22:21:12 +0530

    commiting useState Hook changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,90 @@
 import ResturantCard from "./ResturantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+    //Local state Varairble - Super powerful varaible
+    const [listOfResturants] = useState([{
+        "info": {
+            "id": "11091",
+            "name": "Pizza Hut",
+            "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+            "cuisines": [
+              "Pizzas"
+            ],
+            "avgRating": 3.8,
+            "sla": {
+              "deliveryTime": 33
+            },
+        },
+    },
+    {
+        "info": {
+            "id": "659668",
+            "name": "UBQ by Barbeque Nation",
+            "cloudinaryImageId": "ffe4084febdec52732dffd3624f4e6c1",
+            "cuisines": [
+                "North Indian",
+                "Barbecue",
+                "Biryani",
+                "Kebabs",
+                "Mughlai",
+                "Desserts"
+              ],
+            "avgRating": 4.5,
+            "sla": {
+              "deliveryTime": 30
+            },
+        },
+
+    }]);
+    let listOfResturantsJS = [{
+        "info": {
+            "id": "11091",
+            "name": "Pizza Hut",
+            "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
+            "cuisines": [
+              "Pizzas"
+            ],
+            "avgRating": 3.8,
+            "sla": {
+              "deliveryTime": 33
+            },
+        },
+    },
+    {
+        "info": {
+            "id": "659668",
+            "name": "UBQ by Barbeque Nation",
+            "cloudinaryImageId": "ffe4084febdec52732dffd3624f4e6c1",
+            "cuisines": [
+                "North Indian",
+                "Barbecue",
+                "Biryani",
+                "Kebabs",
+                "Mughlai",
+                "Desserts"
+              ],
+            "avgRating": 4.5,
+            "sla": {
+              "deliveryTime": 30
+            },
+        },
+
+    },
+];
     return (<div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+            <button className="filter-btn" onClick={()=>{
+                //Filter logic
+                listOfResturants = listOfResturants.filter((res)=>
+                    res.info.avgRating>4);
+             console.log(listOfResturants);
+             console.log("button is clicked");
+            }}>Top Rated Resturants</button>
+            </div>
         <div className="res-container">
-            {resList.map((restaurant) => 
+            {listOfResturants.map((restaurant) => 
                 (<ResturantCard key = {restaurant.info.id} resData = {restaurant}/>))};
 
         </div>
