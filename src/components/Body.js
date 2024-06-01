ac5b0bcaff9fa394ad1d61f81f134ce77855519b
kavitha <chkavitha324@gmail.com>
2024-06-02 00:42:37 +0530

    removing the mockdata related changes as we are directly calling API to get data

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,7 +7,7 @@ import resList from "../utils/mockData";
 const Body = () => {
     //Local state Varairble - Super powerful varaible
     //setlistOfResturants is function to change/modify the listOfResturants react varaible
-    const [listOfResturants,setlistOfResturants] = useState(resList);
+    const [listOfResturants,setlistOfResturants] = useState([]);
 
     useEffect(()=>{
         fetchData();
@@ -22,41 +22,6 @@ const Body = () => {
         setlistOfResturants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
     }
     
-    let listOfResturantsJS = [{
-        "info": {
-            "id": "11091",
-            "name": "Pizza Hut",
-            "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
-            "cuisines": [
-              "Pizzas"
-            ],
-            "avgRating": 3.8,
-            "sla": {
-              "deliveryTime": 33
-            },
-        },
-    },
-    {
-        "info": {
-            "id": "659668",
-            "name": "UBQ by Barbeque Nation",
-            "cloudinaryImageId": "ffe4084febdec52732dffd3624f4e6c1",
-            "cuisines": [
-                "North Indian",
-                "Barbecue",
-                "Biryani",
-                "Kebabs",
-                "Mughlai",
-                "Desserts"
-              ],
-            "avgRating": 4.5,
-            "sla": {
-              "deliveryTime": 30
-            },
-        },
-
-    },
-];
     return (<div className="body">
         <div className="filter">
             <button className="filter-btn" onClick={()=>{
