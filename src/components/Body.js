971f74c2f6a9f1ebc97e8f52394ef6ed259f3965
kavitha <chkavitha324@gmail.com>
2024-06-02 12:04:30 +0530

    using the optional chaining of js

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,7 +19,8 @@ const Body = () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
         console.log(json);
-        setlistOfResturants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        //Optional Chaining
+        setlistOfResturants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
     
     return (<div className="body">
