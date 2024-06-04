cd6190dfe5c2630cba4f1d3f07ca6421f32c1153
kavitha <chkavitha324@gmail.com>
2024-06-04 16:27:44 +0530

    login and logout button text change dynamically

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 const Header = ()=>{
+    const [btnReactName,setBtnReactName] = useState("Login");
+
     return(
     <div className="header">
         <div className="log-container">
@@ -11,6 +14,16 @@ const Header = ()=>{
                     <li>About</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="btnName" onClick={()=>{
+                        //Toogle function if btnReactName is login then change to logut otherwise change to login once again
+                        btnReactName == "Login"
+                        ?setBtnReactName("Logout")
+                        :setBtnReactName("Login")
+                        console.log(btnReactName);
+
+                    }
+
+                    }>{btnReactName}</button>
                 </ul>
             </div>
 
