8af4cde986a811ff2e6e83575bec04e569ee7c6e
kavitha <chkavitha324@gmail.com>
2024-06-06 15:50:34 +0530

    Routerprvider and createBrowserRouter changes and added new components to navigate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
  
 const AppLayout = () => {
     return (<div className = "app">
@@ -14,5 +19,22 @@ const AppLayout = () => {
     );
 }
 
+const appRouter = createBrowserRouter([
+    {
+        path:"/",
+        element:<AppLayout/>,
+        errorElement:<Error/>
+    },
+
+    {
+        path:"/about",
+        element:<About/>
+    },
+    {
+        path:"/contact",
+        element:<Contact/>
+    }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<RouterProvider router = {appRouter}/>);
