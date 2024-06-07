79dfba3163d4f7b8289d344cab659dbd9f69f5ee
kavitha <chkavitha324@gmail.com>
2024-06-07 10:33:16 +0530

    userRouterError hook used for showing the proper error

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,13 @@
+import { useRouteError } from "react-router-dom";
+
 const Error = ()=>{
+    const err = useRouteError();
+    console.log(err);
     return (
         <div>
             <h2>Opps!!!</h2>
             <h3>Something went wrong</h3>
+            <h1>{err.status}:{err.statusText}</h1>
         </div>
     );
 }
