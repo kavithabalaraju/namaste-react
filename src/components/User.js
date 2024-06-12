663a323321dc6625ce1b51f5bebb759918e1ab46
kavitha <chkavitha324@gmail.com>
2024-06-12 12:07:27 +0530

    commiting the destructue changes

---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+const User = ({name})=>{
+    return <div className="user-card">
+        <h3>Name:{name}</h3>
+        <h3>Location:Hyderabad</h3>
+        <h4>Contact:chkavitha324</h4>
+    </div>
+}
+
+export default User;
