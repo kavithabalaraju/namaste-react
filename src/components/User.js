c214aee96eccdb3b0a8a36e963d0f1a52361b0f8
kavitha <chkavitha324@gmail.com>
2024-06-12 13:48:12 +0530

    creating state variable inside function and using to update the comp/page

--- src/components/User.js
+++ src/components/User.js
@@ -1,5 +1,11 @@
+import { useState } from "react";
+
 const User = ({name})=>{
+    const [count] = useState(0);
+    console.log(count);
+
     return <div className="user-card">
+        <h1>Count = {count}</h1>
         <h3>Name:{name}</h3>
         <h3>Location:Hyderabad</h3>
         <h4>Contact:chkavitha324</h4>
