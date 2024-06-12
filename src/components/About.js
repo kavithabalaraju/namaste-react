013b554afedf91ace3ad826926dc979991182358
kavitha <chkavitha324@gmail.com>
2024-06-12 12:06:48 +0530

    commiting the argument to child comp from parent

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = ()=>{
         <title>About</title>
         <h1>This is about page</h1>
         <User name={"Kavitha (function)"}/>
-        <UserClass name={"Kavitha (class)"}/>
+        <UserClass name={"Kavitha (class)"} location={"Hyderabad (class)"}/>
     </div>);
 }
 export default About;
