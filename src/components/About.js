f063779709503ef249c7e25f66442c15387c5cc6
kavitha <chkavitha324@gmail.com>
2024-06-12 11:24:26 +0530

    commiting the CB component and FB component changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = ()=>{
     return (<div>
         <title>About</title>
         <h1>This is about page</h1>
+        <User name={"Kavitha (function)"}/>
+        <UserClass name={"Kavitha (class)"}/>
     </div>);
 }
 export default About;
