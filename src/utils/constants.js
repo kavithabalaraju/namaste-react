c202a9da73a346a2f083c24e52c9dd1af5a6d952
kavitha <chkavitha324@gmail.com>
2024-06-12 10:00:42 +0530

    Finding the path related changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,5 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL = "https://images.unsplash.com/photo-1615719413546-198b25453f85?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.4747785&lng=78.3102918&restaurantId=";
