80631cc722dc44fd0036aa1b62554e681db29c31
kavitha <chkavitha324@gmail.com>
2024-06-12 15:14:40 +0530

    updating the state variables in classbased component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,16 +5,22 @@ class UserClass extends React.Component{
         super(props);
         console.log(props);
         this.state = {
-            count:0,
-            count2:2
+            count:0
         };
+
     }
 render(){
    const {name,location} = this.props;
     return (
     <div>
+        <button onClick={()=>{
+            this.setState({
+                count:this.state.count+1
+            })
+        }
+
+        }>Increase Count</button>
         <h1>Count = {this.state.count}</h1>
-        <h1>Count = {this.state.count2}</h1>
         <h3>Name:{name}</h3>
         <h3>Location:{location}</h3>
         <h4>Contact:chkavitha324</h4>
