6b34078053ef206820535c66d68e51d77b560766
kavitha <chkavitha324@gmail.com>
2024-06-13 10:47:32 +0530

    Classbased component life cycle implementation

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,35 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = ()=>{
+class About extends Component{
+    constructor(props){
+        super(props);
+        console.log("Parent constructor");
+    }
+    componentDidMount(){
+        //API call logic
+        console.log("Parent componentDidMount");
+    }
+    render(){
+        console.log("Parent Render");
+        return (<div>
+            <title>About</title>
+            <h1>This is about page</h1>
+            <UserClass name={"Kavitha (class)"} location={"Hyderabad (class)"}/>
+            <UserClass name={"Balaraju (class)"} location={"Hyderabad (class)"}/>
+        </div>);
+
+    }
+
+};
+
+
+/**  const About = ()=>{
     return (<div>
         <title>About</title>
         <h1>This is about page</h1>
         <UserClass name={"Kavitha (class)"} location={"Hyderabad (class)"}/>
-    </div>);
-}
+    </div>); 
+}*/
 export default About;
