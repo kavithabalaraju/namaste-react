743ebf78a19aceec8dd42e3370674ba36617a497
kavitha <chkavitha324@gmail.com>
2024-06-13 14:53:18 +0530

    committing the component life cycle method componentDidUpdateMount and componentWillUnmount

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,37 +2,42 @@ import React from "react";
 
 class UserClass extends React.Component{
     constructor(props){
-        console.log(props.name+ "Child Constructor");
+        console.log("Child Constructor");
         super(props);
-        console.log(props);
+     //   console.log(props);
         this.state = {
-            count:0
+            userInfo:{
+                login:"Dummy",
+                id:"123"
+            }
         };
 
     }
+    async componentDidMount(){
+        //This fun is used for invoke the API call in CBC
+        console.log("Child componentDidMount");
+        const data = await fetch("https://api.github.com/users/kavithabalaraju");
+        const json = await data.json();
+        console.log(json);
+        this.setState({
+            userInfo:json
+        }
+        );
+    }
 render(){
-    console.log(this.props.name+"Child Render");
-   const {name,location} = this.props;
+    console.log("Child Render");
+   const {login,id} = this.state.userInfo;
     return (
     <div>
-        <button onClick={()=>{
-            //Never update the state variable directly (this.state.count = this.state.count+1)
-            this.setState({
-                count:this.state.count+1
-            })
-        }
-
-        }>Increase Count</button>
-        <h1>Count = {this.state.count}</h1>
-        <h3>Name:{name}</h3>
-        <h3>Location:{location}</h3>
+        <h3>Name:{login}</h3>
+        <h3>Id:{id}</h3>
         <h4>Contact:chkavitha324</h4>
     </div>
     )
 }
-componentDidMount(){
-    //This fun is used for invoke the API call in CBC
-    console.log(this.props.name+"Child componentDidMount");
+
+componentDidUpdate(){
+    console.log("componentDidUpdate");
 }
 };
 export default UserClass;
