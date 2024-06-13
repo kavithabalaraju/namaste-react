96c251ed6877de6dcabb8059c4f033d0702ba55b
kavitha <chkavitha324@gmail.com>
2024-06-13 10:48:15 +0530

    Classbased component life cycle implementation

--- src/components/User.js
+++ src/components/User.js
@@ -2,10 +2,12 @@ import { useState } from "react";
 
 const User = ({name})=>{
     const [count] = useState(0);
+    const [count2] = useState(2);
     console.log(count);
 
     return <div className="user-card">
         <h1>Count = {count}</h1>
+        <h1>Count2 = {count2}</h1>
         <h3>Name:{name}</h3>
         <h3>Location:Hyderabad</h3>
         <h4>Contact:chkavitha324</h4>
