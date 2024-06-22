2755d2c837037b58c8053180412b67cb297dec6c
kavitha <chkavitha324@gmail.com>
2024-06-22 14:58:28 +0530

    tailwindcss changes applied styes to appln

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,25 +17,25 @@ const Header = ()=>{
    },[btnReactName])
 
     return(
-    <div className="header">
+    <div className="flex justify-between bg-pink-100 h-32 shadow-lg sm:bg-yellow-50 lg:bg-green-100">
         <div className="log-container">
-                <img className = "logo" src = {LOGO_URL}/>
+                <img className = "w-40 h-32" src = {LOGO_URL}/>
            </div> 
-            <div className="nav-items">
-                <ul>
-                    <li>Online Status:{}</li>
-                    <li><Link to="/">Home</Link></li>
-                    <li>
+            <div className="flex items-center">
+                <ul className="flex m-4 p-10">
+                    <li className="px-4">Online Status:{}</li>
+                    <li className="px-4"><Link to="/">Home</Link></li>
+                    <li className="px-4">
                     <Link to="/about">About Us</Link>
                 
                     </li>
-                    <li>
+                    <li className="px-4">
                         <Link to="/contact">Contact Us</Link>
                         </li>
-                        <li>
+                        <li className="px-4"> 
                             <Link to = "/grocery">Grocery</Link>
                         </li>
-                    <li>Cart</li>
+                    <li className="px-4">Cart</li>
                     <button className="btnName" onClick={()=>{
                         //Toogle function if btnReactName is login then change to logut otherwise change to login once again
                         btnReactName == "Login"
